370bdc6b1990bafee5d87c80a83fb114aebe4153
alexrnov <a.novopashin5@gmail.com>
2019-11-27 23:17:03 +0900

    edit dialog activity

--- app/src/main/java/alexrnov/cosmichunter/GameActivity.java
+++ app/src/main/java/alexrnov/cosmichunter/GameActivity.java
@@ -80,6 +80,18 @@ public class GameActivity extends AppCompatActivity {
     ActionBar ab = getSupportActionBar();
     if (ab != null) ab.hide();
 
+    View decorView = getWindow().getDecorView();
+    int ioOptions = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE ;
+    /*
+    int ioOptions = View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE ;
+            */
+    decorView.setSystemUiVisibility(ioOptions);
+
     // используется в случае полноэкранного режима
     //surfaceView = new SurfaceView(this);
     //setContentView(surfaceView);
--- app/src/main/java/alexrnov/cosmichunter/OGLView.java
+++ app/src/main/java/alexrnov/cosmichunter/OGLView.java
@@ -8,6 +8,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 
 import alexrnov.cosmichunter.utils.commonGL.CoordinatesOpenGL;
 
