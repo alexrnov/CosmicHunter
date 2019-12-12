8e052797a72c27dcc6d1ccc8c7c6d98899252a6c
alexrnov <a.novopashin5@gmail.com>
2019-12-12 23:00:58 +0900

    addition support openGL 2.0

--- app/src/main/assets/shaders/ice_asteroid_f2.glsl
+++ app/src/main/assets/shaders/ice_asteroid_f2.glsl
@@ -2,9 +2,9 @@
 precision lowp float;
 // текстурные координаты(входной параметр из вершинного шейдера)
 // фрагментный шейдер использует их для чтения из текстуры
-attribute vec2 v_textureCoordinates; //in - вместо varying в OpenGL 2.0/GLSL 1.00
-smooth attribute vec4 v_commonLight;
-varying vec4 outColor; // вместо mediump vec4 gl_FragColor в OpenGL 2.0/GLSL 1.00
+varying vec2 v_textureCoordinates; //in - вместо varying в OpenGL 2.0/GLSL 1.00
+varying vec4 v_commonLight;
+//varying vec4 outColor; // вместо mediump vec4 gl_FragColor в OpenGL 2.0/GLSL 1.00
 // сэмплер - специальный тип uniform-переменных, используемых для
 // чтения из текстуры. В эту переменную записывается номер текстурного
 // блока, к которому привязана данная текстура. Текстуры привязываются
@@ -19,6 +19,6 @@ void main() {
     // второй параметр - двухмерные текстурные координаты, используемыя
     // для чтения из текстуры возвращает значение типа vec4,
     // представляющее цвет, прочитанный из текстуры.
-    outColor = texture(s_texture, v_textureCoordinates) * v_commonLight;
-    outColor.a = u_alpha;
+    gl_FragColor = texture2D(s_texture, v_textureCoordinates) * v_commonLight;
+    gl_FragColor.a = u_alpha;
     }
