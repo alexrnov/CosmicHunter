e3e012bb6df62c6481497ee322ac762e5893369d
alexrnov <a.novopashin5@gmail.com>
2020-01-25 10:55:56 +0900

    create espresso test

--- app/src/main/java/alexrnov/cosmichunter/DemoMock.kt
+++
@@ -1,7 +0,0 @@
-package alexrnov.cosmichunter
-
-import android.content.Context
-
-class ClassForDemoMock(val context: Context) {
-  fun printApplicationInfo() = context.getString(R.string.app_name)
-}
