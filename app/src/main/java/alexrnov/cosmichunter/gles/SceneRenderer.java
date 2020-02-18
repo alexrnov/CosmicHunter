aff6b49744d4e79829e2ddbe632d93b47c9783a5
alexrnov <a.novopashin5@gmail.com>
2020-02-18 16:18:14 +0900

    change levels

--- app/src/main/java/alexrnov/cosmichunter/gles/SceneRenderer.java
+++ app/src/main/java/alexrnov/cosmichunter/gles/SceneRenderer.java
@@ -8,6 +8,7 @@ public interface SceneRenderer extends GLSurfaceView.Renderer  {
   /** установить ссылку на gameActivity, чтобы связываться с цифровым табло во время игры */
   void setGameActivity(@NotNull GameActivity gameActivity);
 
+  
   /** передать координаты нажатия на экран */
   void setPassXY(float passX, float passY);
 
