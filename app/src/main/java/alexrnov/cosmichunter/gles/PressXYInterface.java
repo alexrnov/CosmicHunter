b7b1783ddd5a6e4b3157b253648d8a0e81afd974
alexrnov <a.novopashin5@gmail.com>
2020-03-03 14:22:17 +0900

    timer redact

--- app/src/main/java/alexrnov/cosmichunter/gles/PressXYInterface.java
+++ app/src/main/java/alexrnov/cosmichunter/gles/PressXYInterface.java
@@ -1,5 +1,15 @@
 package alexrnov.cosmichunter.gles;
+
+/**
+ * Функциональный интерфейс который позволяет определять алгоритм
+ * обработки нажатия на экран. Если его не использовать придется каждый
+ * рад прибегать к условной проверке в методе setPassXY(float passX, float passY)
+ * для классов Level 1-5
+ * @param <T> координата x нажатия на экран
+ * @param <U> координата y нажатия на экран
+ */
 @FunctionalInterface
 public interface PressXYInterface<T, U> {
+  /** Метод содержит обработчик нажатия на экран */
   void invoke(T x, U y);
 }
