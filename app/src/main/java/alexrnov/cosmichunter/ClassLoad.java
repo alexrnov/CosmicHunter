21f57edaba861c7cc5ba6a3ab4780bb3fdf0d8e6
alexrnov <a.novopashin5@gmail.com>
2020-03-07 16:13:00 +0900

    implements load screen

--- app/src/main/java/alexrnov/cosmichunter/ClassLoad.java
+++ app/src/main/java/alexrnov/cosmichunter/ClassLoad.java
@@ -1,9 +1,10 @@
 package alexrnov.cosmichunter;
 
-import android.graphics.drawable.AnimationDrawable;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
+
+import java.lang.ref.WeakReference;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -12,34 +13,31 @@ import static alexrnov.cosmichunter.Initialization.TAG;
 
 public class ClassLoad extends AsyncTask<Void, Void, Void> {
 
-  private ConstraintLayout bPanel;
-  private AppCompatActivity activity;
+  private WeakReference<ConstraintLayout> bPanel;
+  private WeakReference<AppCompatActivity> activity;
+
   public ClassLoad(AppCompatActivity activity, ConstraintLayout bPanel) {
-    this.activity = activity;
-    this.bPanel = bPanel;
+    this.activity = new WeakReference<>(activity);
+    this.bPanel = new WeakReference<>(bPanel);
   }
 
   @Override
   protected void onPreExecute() {
     Log.i(TAG, "ONPREEXECUTE()");
-    bPanel.setVisibility(View.VISIBLE);
-
+    bPanel.get().setVisibility(View.VISIBLE);
   }
 
   @Override
   protected Void doInBackground(Void... voids) {
-    activity.runOnUiThread(() -> {
+    activity.get().runOnUiThread(() -> {
       // AnimationDrawable animation = (AnimationDrawable) loadImage.getBackground();
       // animation.start(); // выполнить анимацию процесса загрузки
     });
-
     return null;
-
   }
 
   @Override
   protected void onPostExecute(Void result) {
     //bPanel.setVisibility(View.GONE);
   }
-
 }
