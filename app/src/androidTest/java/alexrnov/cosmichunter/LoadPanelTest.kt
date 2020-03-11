d06cf5cd851919fb63e4ee27169efd3615eae912
alexrnov <a.novopashin5@gmail.com>
2020-03-11 09:45:36 +0900

    implements load screen test

--- app/src/androidTest/java/alexrnov/cosmichunter/LoadPanelTest.kt
+++ app/src/androidTest/java/alexrnov/cosmichunter/LoadPanelTest.kt
@@ -87,6 +87,8 @@ class LoadPanelTest {
 
     device.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
             LAUNCH_TIMEOUT) // Wait
+
+    backToMainMenu()
   }
 
   @Test
@@ -112,6 +114,22 @@ class LoadPanelTest {
     }
     device.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
             LAUNCH_TIMEOUT) // Wait
+    backToMainMenu()
+  }
+
+  private fun backToMainMenu() {
     device.pressBack()
+    device.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
+            LAUNCH_TIMEOUT) // Wait
+    val backButton: UiObject = device.findObject(UiSelector()
+            .resourceId("${BASIC_SAMPLE_PACKAGE}:id/button_exit_to_menu"))
+    backButton.clickAndWaitForNewWindow()
+    // возврат в главное меню и проверить видимость компонентов
+    onView(withId(R.id.toolbar_main_menu)).check(matches(isDisplayed()))
+    onView(withId(R.id.startGameButton)).check(matches(isDisplayed()))
+    onView(withId(R.id.levelGameButton)).check(matches(isDisplayed()))
+    onView(withId(R.id.settingsButton)).check(matches(isDisplayed()))
+    onView(withId(R.id.aboutGameButton)).check(matches(isDisplayed()))
+    onView(withId(R.id.exitButton)).check(matches(isDisplayed()))
   }
 }
