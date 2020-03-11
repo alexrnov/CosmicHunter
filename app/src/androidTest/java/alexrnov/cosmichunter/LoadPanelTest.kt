9f39f59f84ff99e7dd2d0dbad169f12293599bb2
alexrnov <a.novopashin5@gmail.com>
2020-03-11 09:14:23 +0900

    implements load screen test

--- app/src/androidTest/java/alexrnov/cosmichunter/LoadPanelTest.kt
+++ app/src/androidTest/java/alexrnov/cosmichunter/LoadPanelTest.kt
@@ -1,20 +1,30 @@
 package alexrnov.cosmichunter
 
+import alexrnov.cosmichunter.Initialization.TAG
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.util.Log
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SdkSuppress
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.*
 import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.not
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.test.uiautomator.UiSelector
+import androidx.test.uiautomator.UiObject
+import com.google.common.truth.Truth.assertThat
+
 
 private const val LAUNCH_TIMEOUT = 3000L
 private const val BASIC_SAMPLE_PACKAGE = "alexrnov.cosmichunter"
@@ -25,6 +35,9 @@ private const val BASIC_SAMPLE_PACKAGE = "alexrnov.cosmichunter"
 class LoadPanelTest {
   private lateinit var device: UiDevice
 
+  private lateinit var loadGameText: String
+  private lateinit var levelNumberText: String
+
   @Before
   fun startMainActivityFromHomeScreen() {
     // Initialize UiDevice instance
@@ -33,7 +46,7 @@ class LoadPanelTest {
     device.pressHome() // Start from the home screen
     // ждем запуска
     val launcherPackage: String = device.launcherPackageName
-    ViewMatchers.assertThat(launcherPackage, CoreMatchers.notNullValue())
+    assertThat(launcherPackage, CoreMatchers.notNullValue())
     device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),
             LAUNCH_TIMEOUT)
 
@@ -47,14 +60,58 @@ class LoadPanelTest {
     context.startActivity(intent)
     device.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
             LAUNCH_TIMEOUT ) // Wait
+
+    loadGameText = context.getString(R.string.load_game_text)
+    levelNumberText = context.getString(R.string.level)
   }
 
   @Test
-  fun startGame() {
+  fun startGameByMainActivity() {
     val startGameButton: UiObject = device.findObject(UiSelector()
             .resourceId("${BASIC_SAMPLE_PACKAGE}:id/startGameButton"))
     startGameButton.click() // начать игру
+
+    // проверить, что панель загрузки отображается
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) { // до Android 7
+      onView(withId(R.id.load_panel_game)).check(matches(isDisplayed()))
+    } else { // начиная с Android 7
+      /* // поиск объекта по тексту
+      val loadGame: UiObject = device
+           .findObject(UiSelector().text(loadGameText))
+       assertThat(loadGame.text).isEqualTo(loadGameText)
+      */
+      val loadGame: UiObject = device.findObject(UiSelector()
+              .resourceId("${BASIC_SAMPLE_PACKAGE}:id/load_game_text"))
+      assertThat(loadGame.text).isEqualTo(loadGameText)
+    }
+
     device.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
             LAUNCH_TIMEOUT) // Wait
+  }
+
+  @Test
+  fun startGameByLevelActivity() {
+    val levelGameButton: UiObject = device.findObject(UiSelector()
+            .resourceId("${BASIC_SAMPLE_PACKAGE}:id/levelGameButton"))
+    levelGameButton.clickAndWaitForNewWindow() // начать игру
+
+    val level1Button: UiObject = device.findObject(UiSelector()
+            .resourceId("${BASIC_SAMPLE_PACKAGE}:id/button_level1"))
+
+    level1Button.click()
+    val firstLevel = "$levelNumberText 1"
+    // проверить, что панель загрузки отображается
+    // (а именно надпись номера уровня на этой панели)
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) { // до Android 7
+      onView(withId(R.id.load_level_text)).check(matches(isDisplayed()))
+      onView(withId(R.id.load_level_text)).check(matches(withText(firstLevel)))
+    } else { // начиная с Android 7
+      val levelN: UiObject = device.findObject(UiSelector()
+              .resourceId("${BASIC_SAMPLE_PACKAGE}:id/load_level_text"))
+      assertThat(levelN.text).isEqualTo(firstLevel)
+    }
+    device.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
+            LAUNCH_TIMEOUT) // Wait
+    device.pressBack()
   }
 }
