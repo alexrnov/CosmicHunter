cc6f886f442e2e8e687b7a4ff612224141d0efe8
alexrnov <a.novopashin5@gmail.com>
2020-05-30 18:30:57 +0900

    change volume for application

--- app/src/main/java/alexrnov/cosmichunter/base/LevelDatabase.java
+++ app/src/main/java/alexrnov/cosmichunter/base/LevelDatabase.java
@@ -3,7 +3,8 @@ package alexrnov.cosmichunter.base;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Level.class}, version = 1)
+// exportSchema = false чтобы не возникала ошибка при сборке apk
+@Database(entities = {Level.class}, version = 1, exportSchema = false)
 public abstract class LevelDatabase extends RoomDatabase {
   public abstract LevelDao levelDao();
 }
